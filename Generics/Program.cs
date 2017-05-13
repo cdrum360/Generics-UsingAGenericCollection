3008e3021e3ad629a646129e34c946e04728a204
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 11:01:46 -0400

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UseTheMagicHat();
+
+            Console.WriteLine("Ta Da!");
+            Console.ReadLine();
+        }
+
+        private static void UseTheMagicHat()
+        {
+ 
+            List<Rabbit> rabbitList = new List<Rabbit>      // intialize List of Rabbit collection
+            {
+                new Rabbit {Name = "Bugs"},
+                new Rabbit {Name = "Roger"},
+                new Rabbit {Name = "Thumper"}
+            };
+
+            var firstRabbitName = rabbitList[0].Name;
+
+            Console.WriteLine(firstRabbitName);
+
+            // rabbitList.Add("Energizer Bunny");     // compile error
+
+            #region Dictionary Example
+
+            //Dictionary<string, Rabbit> rabbitDictionary = new Dictionary<string, Rabbit>();
+
+            //rabbitDictionary.Add("1", new Rabbit { Name = "Bugs" });
+            //rabbitDictionary.Add("2", new Rabbit { Name = "Roger" });
+            //rabbitDictionary.Add("3", new Rabbit { Name = "Thumper" });
+
+            //var firstRabbitName2 = rabbitDictionary["1"].Name;
+            //Console.WriteLine(firstRabbitName2);
+
+            //// rabbitDictionary.Add("4", "Energizer Bunny"});    // compile error
+
+
+            //Console.ReadLine();
+
+            #endregion
+        }
+
+    }
+}
---
+++ Generics/Rabbit.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Generics
+{
+    public class Rabbit
+    {
+        public string Name { get; set; }
+
+        public void EatCarrot()
+        {
+            // ...  illustrative purposes; implementation details are out of scope
+        }
+    }
+}
